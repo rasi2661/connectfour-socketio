1056acb0587da2e57b1248379db36cb51a5e83d1
rasi2661 <70203942+rasi2661@users.noreply.github.com>
2021-06-22 23:35:57 +0200

    add files

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import * as express from "express";
+import * as socketio from "socket.io";
+import * as path from "path";
+
+const app = express();
+app.set("port", process.env.PORT || 3000);
+
+let http = require("http").Server(app);
+let io = require("socket.io")(http);
+
+app.get("/", (req: any, res: any) => {
+  res.sendFile(path.resolve("./client/index.html"));
+});
+
+io.on("connection", function(socket: any) {
+  console.log("a user connected");
+});
+
+const server = http.listen(3000, function() {
+  console.log("Server listening on *:3000");
+});
